177fb997f980b48e3306b9a5a944f4880d8f5c87
Nikhil Clement <smash528@gmail.com>
2019-04-28 12:37:44 +0530

    Added a small comment

--- socket_client.py
+++ socket_client.py
@@ -1,6 +1,7 @@
 import socket
 s = socket.socket()
 port = 11111
+#server is on localhost
 server_ip="127.0.0.1"
 s.connect((server_ip, port))
 s.send(raw_input("Enter the string to be counted for its occurrence\n"))
