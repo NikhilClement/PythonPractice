fee062c50636f9eb8e968a8205cfb28f240d8eb8
Nikhil Clement <smash528@gmail.com>
2019-05-01 20:37:21 +0530

    shell is set to false as recommended by Python standards to avoid security hazards

--- get_and_save_ifconfig.py
+++ get_and_save_ifconfig.py
@@ -1,7 +1,7 @@
 import subprocess
 #subprocess.call("ifconfig")
 f = open("ifconfig.txt", "w+")
-p = subprocess.Popen("ifconfig", stdout = f, shell=True)
+p = subprocess.Popen("ifconfig", stdout = f, shell=False)
 (output, err) = p.communicate()
 f.write(str(output))
 f.close()
